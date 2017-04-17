c2012896269c0d7f87389697b3ddeeda5036e45b
Ju4nPablo <eljuanpi_01@hotmail.com>
2017-04-17 01:11:21 -0500

    hecho todos los rourters

--- HttpService/Routers/apiCombustible.js
+++ HttpService/Routers/apiCombustible.js
@@ -1,20 +1,20 @@
-var express= require('express');
-var router= express.Router();
+var express = require('express');
+var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/tsh';
 var objectId = require('mongodb').ObjectID;
 
-router.post('/saveTipoUsuario',function(req,res){
+router.post('/saveCombustible', function (req, res) {
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         assert.equal(null, err);
 
         console.log(req.body);
-        var collection =db.collection('tipo_usuario');
+        var collection = db.collection('combustible');
         collection.insert(req.body, {
 
-        } );
+        });
 
         res.send('Info ingresada');
 
@@ -23,15 +23,15 @@ router.post('/saveTipoUsuario',function(req,res){
     });
 });
 
-router.get('/getAllTipoUsuario',function(req,res){
+router.get('/getAllCombustible', function (req, res) {
 
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         assert.equal(null, err);
 
         console.log(req.body);
-        var collection =db.collection('tipo_usuario');
+        var collection = db.collection('combustible');
 
-        collection.find().toArray(function(err, results) {
+        collection.find().toArray(function (err, results) {
             console.log(results)
             // send HTML file populated with quotes here
             res.send(results);
--- HttpService/server.js
+++ HttpService/server.js
@@ -13,8 +13,23 @@ app.use(cors());
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.use('/api',require('./Routers/apiUsuario'));
-app.use('/api',require('./Routers/apiTipoUsuario'));
+app.use('/api', require('./Routers/apiUsuario'));
+app.use('/api', require('./Routers/apiTipoUsuario'));
+app.use('/api', require('./Routers/apiTipoCombustible'));
+app.use('/api', require('./Routers/apiTipoEmbarcacion'));
+app.use('/api', require('./Routers/apiTipoMantenimiento'));
+app.use('/api', require('./Routers/apiPropietario'));
+app.use('/api', require('./Routers/apiPuerto'));
+app.use('/api', require('./Routers/apiClientes'));
+app.use('/api', require('./Routers/apiCombustible'));
+app.use('/api', require('./Routers/apiMaterialPetreo'));
+app.use('/api', require('./Routers/apiVehiculos'));
+app.use('/api', require('./Routers/apiUtensilios'));
+app.use('/api', require('./Routers/apiUtensiliosSeleccionados'));
+app.use('/api', require('./Routers/apiProductos'));
+app.use('/api', require('./Routers/apiDetalleMantenimiento'));
+
+
 
 
 app.listen(3000, function(){
